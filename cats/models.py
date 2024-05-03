ae8b5b82238a61c95a92af3772d7c107e071b4a5
unknown <Palmer656@yandex.ru>
2024-05-03 14:31:48 +0300

    финальная версия kittygram2

--- cats/models.py
+++ cats/models.py
@@ -2,11 +2,11 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 CHOICES = (
-    ('Gray', 'Серый'),
-    ('Black', 'Чёрный'),
-    ('White', 'Белый'),
-    ('Ginger', 'Рыжий'),
-    ('Mixed', 'Смешанный'),
+    ("Gray", "Серый"),
+    ("Black", "Чёрный"),
+    ("White", "Белый"),
+    ("Ginger", "Рыжий"),
+    ("Mixed", "Смешанный"),
 )
 
 User = get_user_model()
@@ -24,11 +24,21 @@ class Cat(models.Model):
     color = models.CharField(max_length=16, choices=CHOICES)
     birth_year = models.IntegerField()
     owner = models.ForeignKey(
-        User, related_name='cats', on_delete=models.CASCADE)
-    achievements = models.ManyToManyField(Achievement, through='AchievementCat')
+        User, related_name="cats", on_delete=models.CASCADE
+    )
+    achievements = models.ManyToManyField(
+        Achievement, through="AchievementCat"
+    )
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name", "owner"], name="unique_name_owner"
+            )
+        ]
 
 
 class AchievementCat(models.Model):
@@ -36,4 +46,4 @@ class AchievementCat(models.Model):
     cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.achievement} {self.cat}'
+        return f"{self.achievement} {self.cat}"
--- cats/serializers.py
+++ cats/serializers.py
@@ -11,41 +11,74 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'cats')
-        ref_name = 'ReadOnlyUsers'
+        fields = ("id", "username", "first_name", "last_name", "cats")
+        ref_name = "ReadOnlyUsers"
 
 
 class AchievementSerializer(serializers.ModelSerializer):
-    achievement_name = serializers.CharField(source='name')
+    achievement_name = serializers.CharField(source="name")
 
     class Meta:
         model = Achievement
-        fields = ('id', 'achievement_name')
+        fields = ("id", "achievement_name")
 
 
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
     color = serializers.ChoiceField(choices=CHOICES)
+    # owner = serializers.PrimaryKeyRelatedField(read_only=True)
     age = serializers.SerializerMethodField()
-    
+    owner = serializers.PrimaryKeyRelatedField(
+        read_only=True, default=serializers.CurrentUserDefault()
+    )
+
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'achievements', 'owner',
-                  'age')
+        fields = (
+            "id",
+            "name",
+            "color",
+            "birth_year",
+            "achievements",
+            "owner",
+            "age",
+        )
+        read_only_fields = ("owner",)
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Cat.objects.all(), fields=("name", "owner")
+            )
+        ]
 
     def get_age(self, obj):
         return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
-        if 'achievements' not in self.initial_data:
+        if "achievements" not in self.initial_data:
             cat = Cat.objects.create(**validated_data)
             return cat
         else:
-            achievements = validated_data.pop('achievements')
+            achievements = validated_data.pop("achievements")
             cat = Cat.objects.create(**validated_data)
             for achievement in achievements:
-                current_achievement, status = Achievement.objects.get_or_create(
-                    **achievement)
+                current_achievement, status = (
+                    Achievement.objects.get_or_create(**achievement)
+                )
                 AchievementCat.objects.create(
-                    achievement=current_achievement, cat=cat)
+                    achievement=current_achievement, cat=cat
+                )
             return cat
+
+    def validate_birth_year(self, value):
+        year = dt.date.today().year
+        if not (year - 40 < value <= year):
+            raise serializers.ValidationError("Проверьте год рождения!")
+        return value
+
+    def validate(self, data):
+        if data["color"] == data["name"]:
+            raise serializers.ValidationError(
+                "Имя не может совпадать с цветом!"
+            )
+        return data
